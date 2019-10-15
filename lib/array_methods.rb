0483cc5ca127ffc5705ffdd7419e62a5639a70a8
Michael Gerland <mr.gerland@gmail.com>
2019-10-15 22:35:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,11 @@
-def using_include(array, element)
+def using_include(array, element1)
   using_include = ["wow", "I", "am", "really", "learning", "arrays!"]
   using_include.include?("wow")
+  using_include.include?("Tardis")
+end
+
+def using_include(array, element2)
+  using_include = ["wow", "I", "am", "really", "learning", "arrays!"]
   using_include.include?("Tardis")
 end
 
